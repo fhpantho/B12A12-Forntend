5bdca770702b32c8c2fe3d35558eb36011f7c8d6
Fahim <fahim1020pantho@gmail.com>
2025-12-15 17:51:39 +0600

    added the privet route component

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+
+import { Navigate, Outlet, useLocation } from "react-router";
+import UseAuth from "../hooks/UseAuth";
+
+
+const PrivateRoute = () => {
+  const { user, loading } = UseAuth();
+  const location = useLocation();
+
+ 
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <img
+          src="https://i.gifer.com/ZZ5H.gif"
+          alt="Loading..."
+          className="w-16 h-16 mb-4"
+        />
+        <p className="text-gray-600 font-medium">Checking Authentication...</p>
+      </div>
+    );
+  }
+
+
+   if (!user) {
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
