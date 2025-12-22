e6aa35d5d4b5badae84e99424264219043c8b02e
Fahim <fahim1020pantho@gmail.com>
2025-12-22 23:33:33 +0600

    the webapp is ready for deployement

--- src/hooks/UseAxiosSecure.jsx
+++ src/hooks/UseAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiossecure = axios.create({
-    baseURL : "http://localhost:5000"
+    baseURL : "https://assetsverse.vercel.app"
 })
 
 const UseAxiosSecure = () => {
